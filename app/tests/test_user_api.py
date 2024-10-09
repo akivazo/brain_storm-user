85e5b7bc904d29a3cfc04393d9300f50bc5ce883
Akiva Zonenfeld <akivazo1@gmail.com>
2024-10-09 15:51:33 +0300

    password field

--- app/tests/test_user_api.py
+++ app/tests/test_user_api.py
@@ -16,7 +16,7 @@ def client():
     mock_mongo_client.close()
 
 def create_user(client: FlaskClient):
-    return client.post("/user", json={"name": "John Doe", "email": "john@example.com", "tags": ["tag1", "tag2"]})
+    return client.post("/user", json={"name": "John Doe", "password": "secret", "email": "john@example.com", "tags": ["tag1", "tag2"]})
 
 def test_root(client: FlaskClient):
     response = client.get("/")
@@ -38,25 +38,36 @@ def test_add_user_missing_field(client: FlaskClient):
 def test_get_user(client: FlaskClient):
     response = create_user(client)
     user_id = response.get_json()["id"]
-    response = client.get(f"/user/{user_id}")
+    response = client.get(f"/user/{user_id}/secret")
     assert response.status_code == 200
     data = response.get_json()["user"]
     assert data['name'] == "John Doe"
     assert data['email'] == "john@example.com"
     assert data["tags"] == ["tag1", "tag2"]
+    assert data["password"] == "secret"
+
+def test_get_user_wrond_password(client: FlaskClient):
+    response = create_user(client)
+    user_id = response.get_json()["id"]
+    response = client.get(f"/user/{user_id}/wrond")
+    
+    assert response.status_code == 404
+    data = response.get_json()
+    assert "error" in data
 
 def test_get_user_no_tags(client: FlaskClient):
-    response = client.post("/user", json={"name": "James", "email": "james@example.com"})
+    response = client.post("/user", json={"name": "James", "password": "secret", "email": "james@example.com"})
     user_id = response.get_json()["id"]
-    response = client.get(f"/user/{user_id}")
+    response = client.get(f"/user/{user_id}/secret")
     assert response.status_code == 200
     data = response.get_json()["user"]
     assert data['name'] == "James"
     assert data['email'] == "james@example.com"
     assert data["tags"] == []
+    assert data["password"] == "secret"
 
 def test_get_nonexistent_user(client: FlaskClient):
-    response = client.get("/user/000000000000000000000000")
+    response = client.get("/user/000000000000000000000000/secret")
     assert response.status_code == 404
     data = response.get_json()
     assert "error" in data
@@ -66,5 +77,5 @@ def test_delete_user(client: FlaskClient):
     user_id = response.get_json()
     response = client.delete(f"/user/{user_id}")
     assert response.status_code == 204
-    response = client.get(f"/user/{user_id}")
+    response = client.get(f"/user/{user_id}/secret")
     assert response.status_code == 404
